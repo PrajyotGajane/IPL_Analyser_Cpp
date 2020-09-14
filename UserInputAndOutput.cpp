fd0e7be8ed7b8388cb79972ed94c97b775c76084
PrajyotGajane <prajyotgajane@gmail.com>
2020-09-14 15:59:30 +0530

    Refactored: Option menu to be more clear to the user

--- UserInputAndOutput.cpp
+++ UserInputAndOutput.cpp
@@ -6,9 +6,15 @@ int UserInputAndOutput::selectField()
 {
     int choice;
     cout << "\nSort" << endl;
-    cout << "\n1: Average  2: Strike Rate  3: 4's and 6's";
-    cout << "\n4: Strike Rate and 4's and 6's  5: Average with strike rate  6: Max Runs with Average";
-    cout << "\n7: Bowlers Average  8: Bowlers Strike Rate   9: Bowlers Economy Rate";
+    cout << "\n1: Average";  
+    cout << "\n2: Strike Rate";
+    cout << "\n3: 4's and 6's";
+    cout << "\n4: Strike Rate and 4's and 6's";  
+    cout << "\n5: Average with strike rate";
+    cout << "\n6: Max Runs with Average";
+    cout << "\n7: Bowlers Average";  
+    cout << "\n8: Bowlers Strike Rate";
+    cout << "\n9: Bowlers Economy Rate";
     cout << "\n10: Bowlers Strike rate and 4 and 5 Wkts";
     cout << "\n11: Bowlers Striker rate and average";
     cout << "\n12: Bowlers Wickets and average";
